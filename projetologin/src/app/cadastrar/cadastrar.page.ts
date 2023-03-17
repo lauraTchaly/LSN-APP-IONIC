4c40f2712080753ae1a425fb9a9c50668f4232cf
Natan Coelho <natancoelho21@gmail.com>
2023-03-17 16:39:01 -0300

    TENTATIVA DE LIGAR AS CONFIGURAÇÕES DO BANCO COM O BOTÃO CADASTRAR NA PAGINA DE CADASTRO

--- projetologin/src/app/cadastrar/cadastrar.page.ts
+++ projetologin/src/app/cadastrar/cadastrar.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { usuario } from '../model/usuario.model';
 import { DatabaseService } from '../servico/database.service';
 import { UtilityService } from '../servico/utility.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.page.html',
@@ -12,13 +13,17 @@ export class CadastrarPage implements OnInit {
 
   routeCpf = null;
   usuario: any = {};
+  formGroup: FormGroup;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private banco: DatabaseService,
     private router: Router,
-    private util: UtilityService
+    private util: UtilityService,
+    private formBuilder: FormBuilder
   ) { }
+  
+
 
   ngOnInit() {
 
@@ -26,14 +31,39 @@ export class CadastrarPage implements OnInit {
     if (this.routeCpf){
       this.banco.getOneUsuario(this.routeCpf).subscribe(caixa => {this.usuario = caixa});
     }
+    this.createForm(new usuario());
   }
+   createForm(usuario:usuario){
+     this.formGroup = this.formBuilder.group({
+       email: [usuario.email],
+       celular: [usuario.celular],
+       endereco: [usuario.endereco],
+       cep: [usuario.dataNasc],
+       descricao: [usuario.descricao]
+     })
+   }
+
+
  update(form: any){
    this.banco.updateUsuario(form.value, this.routeCpf);
    this.router.navigate(['/tab3']);
    this.util.toastando("Item Atualizado com sucesso", "middle", 2000, "medium")
  }
- cadastro(){
-   
- }
 
+ cadastro(){
+   let ref = this.usuario.collection('banco.usuario').doc(this.routeCpf)
+   ref.set(this.formGroup.value).then(()=>{
+    console.log('Cadastro do perfil com sucesso');
+    this.router.navigate(['/perfil']);
+    })
+    // .catch(err =>{
+  //    console.log('Erro ao cadatrar')
+  //    console.log(err)
+  //  })
+  
+   
+   
+//this.router.navigate(['/perfil']);
+
+ }
 }
